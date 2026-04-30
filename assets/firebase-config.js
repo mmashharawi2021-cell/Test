fcdf59c1bd69d2210125b49d772e644b8ad813d3
mmashharawi2021-cell <mmashharawi2021@gmail.com>
2026-05-01 00:37:02 +0300

    Add Firebase config template

---
+++ assets/firebase-config.js
@@ -0,0 +1,16 @@
+window.WATER_APP_FIREBASE_CONFIG = {
+  apiKey: "PUT_YOUR_API_KEY_HERE",
+  authDomain: "PUT_YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "PUT_YOUR_PROJECT_ID",
+  storageBucket: "PUT_YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "PUT_YOUR_SENDER_ID",
+  appId: "PUT_YOUR_APP_ID"
+};
+
+window.WATER_APP_SETTINGS = {
+  appName: 'نظام تقارير تشغيل وضخ المياه',
+  defaultUserName: 'صالح الدحنون',
+  defaultRole: 'سوبر أدمن',
+  defaultStationName: 'المحطة الرئيسية',
+  defaultWellName: 'بئر واحد'
+};
